89776bd91e1e32e4368e2219c8a671c085441377
Hugo Phibbs <hugophibbs1@gmail.com>
2024-09-16 17:42:30 +1200

    adding a training constants data class

---
+++ src/training_constants.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+
+G_LRN_RATE_DEFAULT = 0.001
+D_LRN_RATE_DEFAULT = 0.001
+MAX_GRAD_NORM_DEFAULT = 5.0
+MAX_SEQ_LEN_DEFAULT = 200
+BATCH_SIZE_DEFAULT = 32
+EPSILON_DEFAULT = 1e-40
+SEQ_LENGTH_DEFAULT = 256
+NUM_EPOCHS_DEFAULT = 300
+G_HIDDEN_UNITS_DEFAULT = 256
+D_HIDDEN_UNITS_DEFAULT = 256
+G_DROP_PROB_DEFAULT = 0.5
+D_DROP_PROB_DEFAULT = 0.5
+
+
+@dataclass
+class TrainingConstants:
+    """
+    Configuration class for the GAN model.
+
+    I.e. contains hyper params for the model, optimizer, etc.
+    """
+
+    g_learn_rate: float = G_LRN_RATE_DEFAULT
+    d_learn_rate: float = G_LRN_RATE_DEFAULT
+    max_grad_norm: float = MAX_GRAD_NORM_DEFAULT
+    max_seq_length: int = MAX_SEQ_LEN_DEFAULT
+    batch_size: int = BATCH_SIZE_DEFAULT
+    eps: float = EPSILON_DEFAULT
+    seq_length: int = SEQ_LENGTH_DEFAULT
+    num_epochs: int = NUM_EPOCHS_DEFAULT
+    g_hidden_units: int = G_HIDDEN_UNITS_DEFAULT
+    d_hidden_units: int = D_HIDDEN_UNITS_DEFAULT
+    g_drop_prob: float = G_DROP_PROB_DEFAULT
+    d_drop_prob: float = D_DROP_PROB_DEFAULT
+
