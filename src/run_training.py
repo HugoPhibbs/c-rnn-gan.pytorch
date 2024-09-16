3ad7bfcbc307b55d5de77ef488a005ea3bfb9913
Hugo Phibbs <hugophibbs1@gmail.com>
2024-09-16 17:43:39 +1200

    adding a bunch of new args, and functionality to run_training

--- src/run_training.py
+++ src/run_training.py
@@ -16,31 +16,23 @@
 # by Olof Mogren. The referenced code is available in:
 #
 #     https://github.com/olofmogren/c-rnn-gan
-
-import os
 from argparse import ArgumentParser
 
 import torch
-import torch.nn as nn
-from torch import optim
 from torch.utils.data import DataLoader
 
 from c_rnn_gan.src.CRG_model import CRGModel, Generator, Discriminator
 from c_rnn_gan.src.loss import CRGLoss, GLoss, DLoss
 from c_rnn_gan.src.optimizer import CRGOptimizer
 from c_rnn_gan.src.trainer import CRGTrainer
-from utils.data_utils import SingleGameDatasetRandomChoose
+
+import c_rnn_gan.src.training_constants as tc
+from utils.data_utils import DataUtils
+
+from utils.datasets import SingleGameControlDataset
 
 G_FN = 'c_rnn_gan_g.pth'
 D_FN = 'c_rnn_gan_d.pth'
-
-
-MAX_GRAD_NORM = 5.0
-# following values are modified at runtime
-MAX_SEQ_LEN = 200
-BATCH_SIZE = 32
-
-EPSILON = 1e-40  # value to use to approximate zero (to prevent undefined results)
 
 
 class C_RNN_GAN:
@@ -82,6 +74,7 @@ class C_RNN_GAN:
 
     @staticmethod
     def get_instance(num_feats, gen_params, disc_params, optimizer_params, data_loader, num_epochs, num_batches,
+                     training_constants: tc.TrainingConstants,
                      label_smoothing=False, use_cuda=False):
         generator = Generator(num_feats, gen_params["hidden_units"], gen_params["drop_prob"], use_cuda)
         discriminator = Discriminator(num_feats, disc_params["hidden_units"], disc_params["drop_prob"],
@@ -94,9 +87,11 @@ class C_RNN_GAN:
                                  use_sgd=optimizer_params["use_sgd"]
                                  )
 
-        loss = CRGLoss(GLoss(), DLoss(label_smoothing))
+        loss = CRGLoss(GLoss(eps=training_constants.eps), DLoss(label_smoothing))
 
-        trainer = CRGTrainer(model, data_loader, optimizer, loss, num_batches, num_epochs, num_feats, save_g=False,
+        trainer = CRGTrainer(model, data_loader, optimizer, loss, num_batches, num_epochs, num_feats,
+                             training_constants.batch_size, training_constants.max_grad_norm,
+                             training_constants.max_seq_length, save_g=False,
                              save_d=False)
 
         return C_RNN_GAN(model, trainer, data_loader, optimizer, loss)
@@ -120,51 +115,80 @@ def main(args):
     }
 
     optimizer_params = {
-        "g_learn_rate": args.g_lrn_rate,
-        "d_learn_rate": args.d_lrn_rate,
+        "g_learn_rate": args.g_learn_rate,
+        "d_learn_rate": args.d_learn_rate,
         "use_sgd": args.use_sgd
     }
 
-    cols_to_predict = args.cols_to_predict.split(',')
+    training_constants = tc.TrainingConstants(g_learn_rate=args.g_learn_rate, d_learn_rate=args.d_learn_rate,
+                                              batch_size=args.batch_size, eps=args.epsilon, seq_length=args.seq_len,
+                                              num_epochs=args.num_epochs)
 
-    dataset = SingleGameDatasetRandomChoose(args.game_name, frame_count=args.seq_size, cols_to_predict=cols_to_predict)
+    cols_to_keep = args.cols_to_keep.split(',') if args.cols_to_keep is not None else None
+
+    args.train_session_set = DataUtils.read_txt(f"{args.dataset_root}{args.train_session_set}")
+
+    parquet_folder_path = f"{args.dataset_root}/parquet"
+
+    dataset = SingleGameControlDataset(args.game_name, session_set=args.train_session_set, cols_to_keep=cols_to_keep, frame_count=args.seq_len, parquet_folder_path=parquet_folder_path)
 
     data_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
-    crg = C_RNN_GAN.get_instance(dataset.num_pred_features, gen_params, disc_params, optimizer_params, data_loader,
+    crg = C_RNN_GAN.get_instance(dataset.num_features, gen_params, disc_params, optimizer_params, data_loader,
                                  num_epochs=args.num_epochs,
                                  num_batches=args.num_batches,
+                                 training_constants=training_constants,
                                  label_smoothing=args.label_smoothing,
                                  use_cuda=use_cuda)
 
     crg.train()
 
+    # TODO add testing code
+
+
 if __name__ == "__main__":
-    ARG_PARSER = ArgumentParser()
-    ARG_PARSER.add_argument('--load_g', action='store_true')
-    ARG_PARSER.add_argument('--load_d', action='store_true')
-    ARG_PARSER.add_argument('--no_save_g', action='store_true')
-    ARG_PARSER.add_argument('--no_save_d', action='store_true')
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('--load_g', action='store_true', default=False)
+    arg_parser.add_argument('--load_d', action='store_true', default=False)
+    arg_parser.add_argument('--save_g', action='store_true', default=False)
+    arg_parser.add_argument('--save_d', action='store_true', default=False)
+    arg_parser.add_argument("--model_save_path", type=str, help="Path to where models are saved", default=r"C:\Users\hugop\Documents\Uni\Graphics\COMPSCI715\c_rnn_gan\models")
 
-    ARG_PARSER.add_argument('--num_epochs', default=300, type=int)
-    ARG_PARSER.add_argument('--seq_len', default=256, type=int)
-    ARG_PARSER.add_argument('--batch_size', default=16, type=int)
-    ARG_PARSER.add_argument('--g_lrn_rate', default=0.001, type=float)
-    ARG_PARSER.add_argument('--d_lrn_rate', default=0.001, type=float)
+    arg_parser.add_argument('--num_epochs', default=tc.NUM_EPOCHS_DEFAULT, type=int)
+    arg_parser.add_argument('--seq_len', default=tc.SEQ_LENGTH_DEFAULT, type=int)
+    arg_parser.add_argument('--batch_size', default=tc.BATCH_SIZE_DEFAULT, type=int)
+    arg_parser.add_argument('--g_learn_rate', default=tc.G_LRN_RATE_DEFAULT, type=float)
+    arg_parser.add_argument('--d_learn_rate', default=tc.D_LRN_RATE_DEFAULT, type=float)
+    arg_parser.add_argument("--epsilon", default=tc.EPSILON_DEFAULT, type=float)
 
-    ARG_PARSER.add_argument('--no_pretraining', action='store_true')
-    ARG_PARSER.add_argument('--g_pretraining_epochs', default=5, type=int)
-    ARG_PARSER.add_argument('--d_pretraining_epochs', default=5, type=int)
+    arg_parser.add_argument('--g_hidden_units', default=tc.G_HIDDEN_UNITS_DEFAULT, type=int)
+    arg_parser.add_argument('--d_hidden_units', default=tc.D_HIDDEN_UNITS_DEFAULT, type=int)
+    arg_parser.add_argument('--g_drop_prob', default=tc.G_DROP_PROB_DEFAULT, type=float)
+    arg_parser.add_argument('--d_drop_prob', default=tc.D_DROP_PROB_DEFAULT, type=float)
+
+    arg_parser.add_argument('--no_pretraining', action='store_true', default=False)
+    arg_parser.add_argument('--g_pretraining_epochs', default=5, type=int)
+    arg_parser.add_argument('--d_pretraining_epochs', default=5, type=int)
     # ARG_PARSER.add_argument('--freeze_d_every', default=5, type=int)
-    ARG_PARSER.add_argument('--use_sgd', action='store_true')
-    ARG_PARSER.add_argument('--conditional_freezing', action='store_true')
-    ARG_PARSER.add_argument('--label_smoothing', action='store_true')
-    ARG_PARSER.add_argument('--feature_matching', action='store_true')
+    arg_parser.add_argument('--use_sgd', action='store_true', default=False)
+    arg_parser.add_argument('--conditional_freezing', action='store_true', default=False)
+    arg_parser.add_argument('--label_smoothing', action='store_true', default=False)
+    arg_parser.add_argument('--feature_matching', action='store_true', default=False)
 
-    ARG_PARSER.add_argument("--col_to_predict", type=str, help="Comma-separated list argument")
+    arg_parser.add_argument("--num_batches", type=int, help="Number of batches to train on", default=1000)
 
-    ARGS = ARG_PARSER.parse_args()
-    MAX_SEQ_LEN = ARGS.seq_len
-    BATCH_SIZE = ARGS.batch_size
+    # For data loading
+    arg_parser.add_argument("--dataset_root", type=str, help="Root directory of dataset",
+                            default=r"C:\Users\hugop\Documents\Uni\Graphics\COMPSCI715\datasets")
 
-    main(ARGS)
+    # For Single Game Training
+    arg_parser.add_argument("--game_name", type=str, help="Game name to run training on", default='Barbie')
+    arg_parser.add_argument("--train_session_set", type=str, help=".txt file path containing list of training sessions",
+                            default=r"\barbie_demo_dataset\train.txt")
+    arg_parser.add_argument("--cols_to_keep", type=str, help="Comma-separated list argument of columns to keep", default="hand_trigger_left,hand_trigger_right")
+
+    args = arg_parser.parse_args()
+
+    print(args)
+
+    main(args)
